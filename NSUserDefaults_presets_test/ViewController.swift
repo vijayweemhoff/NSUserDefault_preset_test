2d4dde5a8b4bd94016d266929aee360b1eb2d3c6
Vijay Weemhoff <vijay.w@hotmail.com>
2016-06-03 16:12:09 +0200

    Made a simple background set Preset for example

--- NSUserDefaults_presets_test/ViewController.swift
+++ NSUserDefaults_presets_test/ViewController.swift
@@ -9,10 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let userDefaults = NSUserDefaults.standardUserDefaults()
 
+    @IBOutlet var segColour: UISegmentedControl!
+    
+    @IBAction func actionColour(sender: UISegmentedControl) {
+        switch(segColour.selectedSegmentIndex){
+        case 0:
+            self.view.backgroundColor = UIColor.redColor()
+        case 1:
+            self.view.backgroundColor = UIColor.blueColor()
+        case 2:
+            self.view.backgroundColor = UIColor.greenColor()
+        default:
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+        
+    }
+    
+    @IBAction func saveButtonPressed(sender: UIButton) {
+        let bgColourNo = segColour.selectedSegmentIndex
+        
+        userDefaults.setObject(bgColourNo, forKey: "bgColour")
+        userDefaults.synchronize()
+    }
+    
+    @IBAction func setDefaultPressed(sender: AnyObject) {
+        let bgColourNo: Int? = userDefaults.objectForKey("bgColour") as! Int?
+        
+        switch(bgColourNo!){
+        case 0:
+            self.view.backgroundColor = UIColor.redColor()
+            self.segColour.selectedSegmentIndex = 0
+        case 1:
+            self.view.backgroundColor = UIColor.blueColor()
+            self.segColour.selectedSegmentIndex = 1
+        case 2:
+            self.view.backgroundColor = UIColor.greenColor()
+            self.segColour.selectedSegmentIndex = 2
+        default:
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var bgColourNo: Int? = userDefaults.objectForKey("bgColour") as! Int?
+        if(bgColourNo==nil){
+            bgColourNo=1
+            userDefaults.setObject(bgColourNo, forKey: "bgColour")
+        }
+        switch(bgColourNo!){
+        case 0:
+            self.view.backgroundColor = UIColor.redColor()
+            self.segColour.selectedSegmentIndex = 0
+        case 1:
+            self.view.backgroundColor = UIColor.blueColor()
+            self.segColour.selectedSegmentIndex = 1
+        case 2:
+            self.view.backgroundColor = UIColor.greenColor()
+            self.segColour.selectedSegmentIndex = 2
+        default:
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
