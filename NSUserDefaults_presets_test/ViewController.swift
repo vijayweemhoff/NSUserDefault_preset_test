25ee003774e9d69343039fd55f3ddea05bd89077
Vijay Weemhoff <vijay.w@hotmail.com>
2016-06-04 20:44:33 +0200

    new mutableArray code + save + recall funcions

--- NSUserDefaults_presets_test/ViewController.swift
+++ NSUserDefaults_presets_test/ViewController.swift
@@ -10,6 +10,98 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var bgColourNo: Int? = userDefaults.objectForKey("bgColour") as! Int?
+        if(bgColourNo==nil){
+            bgColourNo=1
+            userDefaults.setObject(bgColourNo, forKey: "bgColour")
+        }
+       
+        switch(bgColourNo!){
+        case 0:
+            self.view.backgroundColor = UIColor.redColor()
+            self.segColour.selectedSegmentIndex = 0
+        case 1:
+            self.view.backgroundColor = UIColor.blueColor()
+            self.segColour.selectedSegmentIndex = 1
+        case 2:
+            self.view.backgroundColor = UIColor.greenColor()
+            self.segColour.selectedSegmentIndex = 2
+        default:
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //
+    //nieuwe code voor interface, makkelijker en meer bugproof
+    //en mogelijkheid voor inladen van presets
+    
+    var mutArray : NSMutableArray = ["List is empty"]
+    
+    @IBOutlet var showElementInArray: UITextField!
+    
+    @IBAction func putRandomInArray(sender: UIButton) {
+        let randomMIDI = Int(arc4random_uniform(126)+1);
+        mutArray.addObject(randomMIDI)
+    }
+    
+    var showArray=0
+    
+    func loopInList(){
+        if(showArray>=mutArray.count-1){
+            showArray=0
+        }
+        
+        showArray=showArray+1
+    }
+    
+    @IBAction func tapButtonPressed(sender: UIButton) {
+        if(mutArray.count==1){
+            //NSLog("List is empty")
+            showElementInArray.text = "List is empty"
+        }
+        if(mutArray.count>1){
+            loopInList()
+            showElementInArray.text = String(mutArray[showArray]) 
+        }
+        
+    }
+    
+    @IBAction func clearArrayPressed(sender: UIButton) {
+        mutArray.removeAllObjects();
+        mutArray.addObject("List is empty");
+    }
+    
+    @IBAction func deleteLastObjectPressed(sender: UIButton) {
+        if(mutArray.count>1){
+            mutArray.removeLastObject();
+        }
+        else{
+            showElementInArray.text = "Can't delete anymore"
+        }
+    }
+    
+    @IBAction func saveListButtonPressed(sender: UIButton) {
+        NSUserDefaults.standardUserDefaults().setObject(mutArray, forKey: "first array")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    @IBAction func recallListButtonPressed(sender: UIButton) {
+        if let tempNames: NSArray = NSUserDefaults.standardUserDefaults().arrayForKey("first array") {
+            mutArray = tempNames.mutableCopy() as! NSMutableArray
+        }
+    }
+    //Einde nieuwe code for interface
+    //
+    //
+    
     let userDefaults = NSUserDefaults.standardUserDefaults()
 
     @IBOutlet var segColour: UISegmentedControl!
@@ -52,34 +144,6 @@ class ViewController: UIViewController {
             self.view.backgroundColor = UIColor.whiteColor()
         }
 
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        var bgColourNo: Int? = userDefaults.objectForKey("bgColour") as! Int?
-        if(bgColourNo==nil){
-            bgColourNo=1
-            userDefaults.setObject(bgColourNo, forKey: "bgColour")
-        }
-        switch(bgColourNo!){
-        case 0:
-            self.view.backgroundColor = UIColor.redColor()
-            self.segColour.selectedSegmentIndex = 0
-        case 1:
-            self.view.backgroundColor = UIColor.blueColor()
-            self.segColour.selectedSegmentIndex = 1
-        case 2:
-            self.view.backgroundColor = UIColor.greenColor()
-            self.segColour.selectedSegmentIndex = 2
-        default:
-            self.view.backgroundColor = UIColor.whiteColor()
-        }
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
